00dd820246dfcbae73abaa42b78a756e9e2ef41b
Taahh <thepheonixblader@gmail.com>
2024-12-22 18:01:43 -0800

    express.js

---
+++ server/config.yml.ex
@@ -0,0 +1,14 @@
+##############################
+# BlueHearts Automation v1.0 #
+##############################
+
+
+server:
+  address: "localhost"
+  port: 3001
+  
+  # not implemented yet
+  ssl:
+    enabled: false
+    cert: "/path/to/cert.pem"
+    privateKey: "/path/to/private_key.pem"
