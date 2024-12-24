464bbf16fd1f130a2188a2cdcd1e3ea361d8a9d2
Kavin Phabiani <kavinphab@gmail.com>
2024-12-23 17:17:51 -0800

    inital changes to yaml file waiting on clerk keys

--- server/src/routes/api/test/route.ts
+++ server/src/routes/api/test/route.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 
 export const GET = async (request: Request, response: Response) => {
-    response.send("hi").status(200);
-}
+  response.send("hi").status(200);
+};
 
 export const POST = async (request: Request, response: Response) => {
-    response.send("hi").status(200);
-}
+  response.send("hi").status(200);
+};
