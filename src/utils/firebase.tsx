5bff9d3c6b2e638e1e7a178dec2fc592a0cca8cf
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-01-14 19:09:56 -0800

    idk

---
+++ src/utils/firebase.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+// import { useAuth } from "@clerk/nextjs";
+// import { signInWithCustomToken } from "firebase/auth";
+// import { auth } from "../../server/utils/firebase";
+
+export const signinAuth = async () => {
+  // const { getToken } = useAuth();
+  // try {
+  //   const token = await getToken({ template: "integration_firebase" });
+  //   if (!token) {
+  //     console.error("No token retrieved from Clerk.");
+  //     return;
+  //   }
+  //   const userCredentials = await signInWithCustomToken(auth, token);
+  //   console.log("Firebase User:", userCredentials.user);
+  // } catch (error) {
+  //   console.error("Error signing in with Firebase:", error);
+  // }
+};
+
+export const signupAuth = () => {};
