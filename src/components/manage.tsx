727faee14b0e93caa1861de0bccae312e91be71f
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2025-01-20 00:32:49 -0800

    header editable

--- src/components/manage.tsx
+++ src/components/manage.tsx
@@ -4,7 +4,7 @@ import OrgHeader from "./org-header";
 const Manage = () => {
   return (
     <div className="flex flex-col items-center w-10/12 m-10 gap-8">
-      <OrgHeader />
+      <OrgHeader editable />
       <Information />
     </div>
   );
--- src/components/org-header.tsx
+++ src/components/org-header.tsx
@@ -1,27 +1,56 @@
+"use client";
 import mock from "@/public/user/mock.png";
 import Image from "next/image";
 import { link } from "@/data/mockDashboard";
 import Link from "next/link";
-import { Link2 } from "lucide-react";
-const OrgHeader = () => {
+import { Link2, Pencil } from "lucide-react";
+import { useState } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+type props = {
+  editable: boolean;
+};
+const OrgHeader = ({ editable }: props) => {
+  const [edit, setEdit] = useState<boolean>(false);
   return (
-    <div>
+    <div className="flex flex-row justify-between w-full">
       <div className="flex justify-start w-full gap-4">
         <Image src={mock} alt="logo" />
         <div>
           <h1 className="text-3xl font-extrabold">POOR MAGAZINE</h1>
-          {link.map((link, index) => (
-            <Link
-              key={index}
-              href={link.url}
-              className="flex gap-2 hover:opacity-40"
-            >
-              <Link2 />
-              <span className="font-bold">{link.name}</span>
-            </Link>
-          ))}
+          {!edit &&
+            link.map((link, index) => (
+              <Link
+                key={index}
+                href={link.url}
+                className="flex gap-2 hover:opacity-40"
+              >
+                <Link2 />
+                <span className="font-bold">{link.name}</span>
+              </Link>
+            ))}
+          {edit &&
+            link.map((link, index) => (
+              <div key={index} className="flex flex-row items-center gap-2">
+                <Link2 />
+                <Input
+                  value={link.name}
+                  className="border-0 border-b shadow-none"
+                />
+              </div>
+            ))}
         </div>
       </div>
+      {!edit && editable && <Pencil onClick={() => setEdit(!edit)} />}
+      {edit && (
+        <Button
+          onClick={() => setEdit(!edit)}
+          className="bg-ttickles-blue text-white shadow-none hover:bg-ttickles-blue"
+        >
+          Save
+        </Button>
+      )}
     </div>
   );
 };
