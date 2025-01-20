235a85d362a259e149c34120d31e3273914d52d6
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2025-01-20 01:12:29 -0800

    yuh

--- src/app/orgs/[orgId]/manage/page.tsx
+++ src/app/orgs/[orgId]/manage/page.tsx
@@ -4,9 +4,7 @@ import Manage from "@/components/manage";
 const Page = () => {
   return (
     <div className="flex w-full justify-center">
-      <div className="flex flex-col items-center w-10/12 m-10 gap-8">
-        <Manage />
-      </div>
+      <Manage />
     </div>
   );
 };
