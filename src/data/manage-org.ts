1e196482b5a38eb0dcd74e39090902d1ec94c7e8
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2025-01-20 18:16:30 -0800

    frontend with functionality finished

---
+++ src/data/manage-org.ts
@@ -0,0 +1,14 @@
+export const QUESTIONS = [
+  {
+    name: "Description",
+    type: "textarea",
+  },
+  {
+    name: "Location",
+    type: "input",
+  },
+  {
+    name: "Theme",
+    type: "input",
+  },
+];
