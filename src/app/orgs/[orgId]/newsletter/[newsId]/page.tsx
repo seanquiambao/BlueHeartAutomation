8029e7339c54a7faf25ae23df3ae92c46ca67f6f
seanquiambao <slquiambao03@gmail.com>
2025-01-27 13:37:09 -0800

    frontend done

--- src/app/orgs/[orgId]/newsletter/[newsId]/page.tsx
+++ src/app/orgs/[orgId]/newsletter/[newsId]/page.tsx
@@ -1,5 +1,4 @@
 import Newsletter from "@/components/newsletter/newletter";
-
 const Page = () => {
   return (
     <div className="flex flex-col w-10/12 m-10">
--- src/components/newsletter/newletter.tsx
+++ src/components/newsletter/newletter.tsx
@@ -1,21 +1,8 @@
-"use client";
-import React, { useState, ChangeEvent } from "react";
 import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
 import { Label } from "../ui/label";
-import { NewsletterType } from "@/types/newsletter";
-import { HTMLInputs } from "@/types/inputs";
 
-type props = {
-  newsletter: NewsletterType;
-  setNewsletter: (value: NewsletterType) => void;
-};
-const Newsletter = ({ newsletter, setNewsletter }: props) => {
-  const [prompt, setPrompt] = useState<string>("");
-
-  const handleChange = (e: ChangeEvent<HTMLInputs>, key: string) => {
-    setNewsletter({ ...newsletter, [key]: e.target.value });
-  };
+const Newsletter = () => {
   return (
     <div className="flex flex-col gap-4">
       <div className="font-extrabold text-3xl mb-8">Newsletter</div>
@@ -23,7 +10,7 @@ const Newsletter = ({ newsletter, setNewsletter }: props) => {
         <Label className="font-bold">
           Write a prompt for newsletter generation
         </Label>
-        <Textarea value={prompt} onChange={(e) => setPrompt(e.target.value)} />
+        <Textarea />
       </div>
 
       <Button className="border-2 hover:bg-slate-200 w-1/5">
@@ -31,10 +18,7 @@ const Newsletter = ({ newsletter, setNewsletter }: props) => {
       </Button>
       <div>
         <Label className="font-bold">Optimized Newsletter</Label>
-        <Textarea
-          value={newsletter.body}
-          onChange={(e) => handleChange(e, "body")}
-        />
+        <Textarea />
       </div>
     </div>
   );
