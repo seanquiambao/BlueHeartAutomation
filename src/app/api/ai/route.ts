dcbb7d64a302a492fdf2614f74a254265a555565
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-01-28 21:57:37 -0800

    fix build

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import Together from "together-ai";
 
 const together = new Together({
-  apiKey: process.env.TOGETHER_AI_KEY,
+  apiKey: process.env.TOGETHER_AI_KEY || "",
 });
 
 export const POST = async (req: NextRequest) => {
