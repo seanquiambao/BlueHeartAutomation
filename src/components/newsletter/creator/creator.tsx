be0679d09f5520a7aa69240cf3d7ff1e414d4d41
seanquiambao <slquiambao03@gmail.com>
2025-01-28 17:03:02 -0800

    cleanup

--- src/components/newsletter/creator/creator.tsx
+++ src/components/newsletter/creator/creator.tsx
@@ -1,20 +1,15 @@
 import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
-import Suggestion from "./suggestions";
-import { SUGGESTIONS } from "@/data/newsletter/suggestions";
+import Prompt from "./prompt";
 import Events from "./events";
 const Creator = () => {
   return (
     <div className="flex flex-col gap-4 h-full">
       <div className="font-extrabold text-3xl mb-8">Newsletter</div>
       <div className="flex flex-row h-full gap-2">
-        <div className="flex flex-col bg-black/5 p-4 rounded-md border border-black/20 w-full gap-2 h-full">
+        <div className="flex flex-col bg-black/5 p-4 rounded-md border border-black/20 w-full gap-4 h-full">
           <Textarea className="resize-none border-black/20 bg-white h-full" />
-          <div className="flex flex-row gap-2">
-            {SUGGESTIONS.map((suggestion, index) => (
-              <Suggestion text={suggestion} key={index} />
-            ))}
-          </div>
+          <Prompt text="write a paragraph about an interview with Drew Phillips and Enya Umanzor" />
           <Input
             placeholder="write your prompt here"
             className="placeholder:text-black/20 placeholder:font-bold bg-white"
--- src/data/newsletter/suggestions.ts
+++
@@ -1,4 +0,0 @@
-export const SUGGESTIONS = [
-  "Write a paragraph about an interview with Drew phillips and Enya Umanzor",
-  "Write a paragraph about the new organization called POOR Magazine",
-];
