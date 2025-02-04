def7e7b8c851b29d485d0743a54bd851b8fcb85e
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-02-03 17:12:02 -0800

    added some animationa dn metadata

--- src/app/orgs/[orgId]/layout.tsx
+++ src/app/orgs/[orgId]/layout.tsx
@@ -1,5 +1,11 @@
 import Navigation from "@/components/global/navigation";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Organizations",
+  description: "The Organizations Dashboard for Ttickle",
+};
 
 type LayoutProps = {
   children: React.ReactNode;
