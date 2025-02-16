ba20bfe8f741366bae48b9b9338a1ce642675931
Taah <admin@taah.dev>
2025-02-16 02:38:05 -0800

    user repository, org repository, routes, create org component functionality

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,20 +1,7 @@
 "use client";
-import { User } from "@clerk/nextjs/server";
-import { useEffect, useState } from "react";
 import OrganizationForm from "@/components/joinorg";
 
 const Page = () => {
-  // const clerk = await clerkClient() // server-side method
-  // const { userId } = await auth();
-  // const user = await clerk.users.getUser(userId ?? "")
-  // const [user, setUser] = useState<User>();
-  // useEffect(() => {
-  //   fetch("/api/user").then((resp) => {
-  //     resp.json().then((json) => setUser(json["message"]));
-  //   });
-  // }, []);
-  // console.log("page")
-  // console.log(user);
   return (
     <div className="w-full flex">
       <OrganizationForm />
